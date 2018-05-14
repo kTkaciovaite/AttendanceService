59d1104b10fb18a871dd331a0a903862824078a3
Karolina Tkačiovaitė <karolina.tkaciovaite@gmail.com>
2018-05-14 22:32:47 +0300

    Login page update

--- AttendanceService/AttendanceService.WebApp/ViewModels/AccountViewModels.cs
+++ AttendanceService/AttendanceService.WebApp/ViewModels/AccountViewModels.cs
@@ -5,16 +5,13 @@ namespace AttendanceService.WebApp.ViewModels
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [Display(Name = "Remember me?")]
+        
         public bool RememberMe { get; set; }
     }
 }
